e3edaf0bae378765879f527fd10612d69e4606a6
kahyalar <furkan.kahyalar@gmail.com>
2017-12-19 00:01:44 +0300

    Initial Commit

---
+++ Onubuna/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Onubuna
+//
+//  Created by Furkan Kahyalar on 19/12/2017.
+//  Copyright © 2017 kahyalar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
