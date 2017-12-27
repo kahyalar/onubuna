3e8b9cbd8ae4029d3a4680afa4137a1eaed53c51
kahyalar <furkan.kahyalar@gmail.com>
2017-12-27 16:24:57 +0300

    Picker views added

--- Onubuna/ViewController.swift
+++ Onubuna/ViewController.swift
@@ -7,19 +7,69 @@
 //
 
 import UIKit
+import Alamofire
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource{
 
+    @IBOutlet weak var inputCurrency: UITextField!
+    @IBOutlet weak var inputAmount: UITextField!
+    @IBOutlet weak var outputCurrency: UITextField!
+    @IBOutlet weak var outputAmount: UITextField!
+    @IBOutlet weak var buttonConvert: UIButton!
+    
+    let inputPickerView = UIPickerView()
+    let outputPickerView = UIPickerView()
+    let currencies = ["TRY", "EUR", "USD", "BTC", "ETH"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        disableOutputAmountTextField()
+        createPickerView()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func disableOutputAmountTextField(){
+        outputAmount.isUserInteractionEnabled = false
     }
-
+    
+    func createPickerView(){
+        inputPickerView.delegate = self
+        outputPickerView.delegate = self
+        inputCurrency.inputView = inputPickerView
+        outputCurrency.inputView = outputPickerView
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return currencies.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return currencies[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if pickerView == inputPickerView{
+            inputCurrency.text = currencies[row]
+        }
+        else{
+            outputCurrency.text = currencies[row]
+        }
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func actionConvert(_ sender: UIButton) {
+        debugPrint("Tık")
+    }
+    
+    
+    
 
 }
 
